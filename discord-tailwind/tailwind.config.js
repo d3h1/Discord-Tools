f382f42486516a379a07391039ac3e277e9f8867
d3h1 <denicabaravdic@gmail.com>
2023-04-10 10:54:17 -0700

    Recreating discord

--- discord-tailwind/tailwind.config.js
+++ discord-tailwind/tailwind.config.js
@@ -4,13 +4,13 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   mode: 'jit',
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         primary: '#202225',
         secondary: '#5865f2',
-        gray: colors.trueGray,
+        gray: colors.neutral,
         gray: {
           900: '#202225',
           800: '#2f3136',
